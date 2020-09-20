2e49698dfdece28dd1f0dbc0db57e258addb53b6
Abdulaziz <aboal3z2013@gmail.com>
2020-09-20 12:15:57 +0300

    master

---
+++ Front-end/src/_actions/types.js
@@ -0,0 +1,10 @@
+export const LOGIN_USER = 'login_user';
+export const REGISTER_USER = 'register_user';
+export const AUTH_USER = 'auth_user';
+export const LOGOUT_USER = 'logout_user';
+
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLESR_ERRORS = 'CLESR_ERRORS';
